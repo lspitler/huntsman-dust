c47d43ed0b23b5d211efdf8ecc750afd015786a9
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-18 17:20:51 +0530

    Fits image is now opened in a linear and logarithimic colour scale. Each version of the image is opened in a seperate window. Comments added to make the code easier to understand and autopep8 formatting implemented

--- read_fits.py
+++ read_fits.py
@@ -3,20 +3,34 @@
 import os
 import glob
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 from astropy.io import fits
 
+
+# Finds FITs files and created image_path
 cwd = os.getcwd()
 data_dir = '/Users/amanchokshi/Desktop/Huntsman/Data'
 os.chdir(data_dir)
 for file in glob.glob("*.fits"):
     print(file)
 os.chdir(cwd)
-    
-image = os.path.join(data_dir, file)
-hdulist = fits.open(image)
-hdu = hdulist[0]
+image_path = os.path.join(data_dir, file)
 
-print(hdu.data.shape)
+# Read in FITs file
+hdulist = fits.open(image_path)
 
-plt.imshow(hdu.data, origin='lower')
+# Extract image and header
+image = hdulist[0].data
+header = hdulist[0].data
+
+# print(hdu.data.shape)
+# print(hdu.header)
+
+plt.figure(1)
+plt.imshow(image, origin='lower', cmap='gray')
+plt.colorbar()
+
+plt.figure(2)
+plt.imshow(image, origin='lower', cmap='gray', norm=LogNorm())
+plt.colorbar()
 plt.show()
