4a7bb2ecc12e6a47ef9da92e73af3b9bef8e10e5
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-20 22:18:24 +0530

    Corrected an error in line 56 - bkgrms. It was a median value instead of creating an array, this was fixed in this version

--- source_detect.py
+++ source_detect.py
@@ -17,7 +17,7 @@ from astropy.stats import SigmaClip
 from photutils import detect_threshold
 from photutils import detect_sources
 from photutils import Background2D, MedianBackground
-from photutils import BiweightScaleBackgroundRMS
+from photutils import deblend_sources
 
 
 # Finds FITs files and created image_path
@@ -53,7 +53,7 @@ print(bkg.background_median)
 print(bkg.background_rms_median)
 plt.imshow(bkg.background, origin='lower', cmap='Greys')
 bkg.plot_meshes(outlines=True, color='#1f77b4')
-bkgrms = bkg.background_rms_median
+bkgrms = bkg.background_rms
 
 # detects a threshold of 3 sigma above background
 # threshold = detect_threshold(image, snr=3.)
@@ -62,11 +62,12 @@ threshold = bkg.background + (3.0 * bkgrms)
 sigma = 2.0 * gaussian_fwhm_to_sigma    # FWHM = 2.
 kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
 kernel.normalize()
-segm = detect_sources(image, threshold, npixels=5, filter_kernel=kernel)
+segm = detect_sources(image, threshold, npixels=6, filter_kernel=kernel)
+
 
 norm = ImageNormalize(stretch=SqrtStretch())
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 7))
-ax1.imshow(image, origin='lower', cmap='Greys', norm=LogNorm())
+ax1.imshow(image, origin='lower', cmap='Greys_r', norm=LogNorm())
 ax2.imshow(segm, origin='lower', cmap=segm.cmap(random_state=12345))
 
 
