6f2d86834ba070393e9fc1bf7bcf6769ba1c9a43
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-20 19:29:45 +0530

    Changed the threshold form detect_threshold() to a manually created one. This was done by generating a 2D background and them applying a 3-sigma threshold. The 2D background used 20x20 pixel boxes. It also used a mask since the image file seems to be a mosaic.

--- source_detect.py
+++ source_detect.py
@@ -2,16 +2,22 @@
 
 import os
 import glob
+
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+
 from astropy.io import fits
 from astropy.wcs import WCS
-from photutils import detect_threshold
 from astropy.convolution import Gaussian2DKernel
 from astropy.stats import gaussian_fwhm_to_sigma
-from photutils import detect_sources
 from astropy.visualization import SqrtStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
+from astropy.stats import SigmaClip
+
+from photutils import detect_threshold
+from photutils import detect_sources
+from photutils import Background2D, MedianBackground
+from photutils import BiweightScaleBackgroundRMS
 
 
 # Finds FITs files and created image_path
@@ -30,13 +36,28 @@ hdulist = fits.open(image_path)
 image = hdulist[0].data
 header = hdulist[0].header
 
-image = image[1500:2500, 2000:3000].astype(float)
+# Selects sub image
+image = image[1600:2300, 2000:3000].astype(float)
 
 # reads header and creates World Coordinate System (WCS) object
 w = WCS(header)
 
+# 2D background estimation
+sigma_clip = SigmaClip(sigma=3., iters=10)
+mask = (image == 0)
+bkg_estimator = MedianBackground()
+bkg = Background2D(image, (20, 20), filter_size=(10, 10),
+                   sigma_clip=sigma_clip, bkg_estimator=bkg_estimator,
+                   mask=mask, edge_method=u'pad')
+print(bkg.background_median)
+print(bkg.background_rms_median)
+plt.imshow(bkg.background, origin='lower', cmap='Greys')
+bkg.plot_meshes(outlines=True, color='#1f77b4')
+bkgrms = bkg.background_rms_median
+
 # detects a threshold of 3 sigma above background
-threshold = detect_threshold(image, snr=3.)
+# threshold = detect_threshold(image, snr=3.)
+threshold = bkg.background + (3.0 * bkgrms)
 
 sigma = 2.0 * gaussian_fwhm_to_sigma    # FWHM = 2.
 kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
