fd82dbf5eca0641a4be28d4eb2148b6bdf30474d
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-21 18:21:43 +0530

    Fixed hard-coded values of argument in previous version

--- background_2D.py
+++ background_2D.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 from astropy.stats import SigmaClip
 from photutils import Background2D, MedianBackground
 
-def background_2D(image, sigma=3, iters=10, boxsize=20, filter_size=10):
-    sigma_clip = SigmaClip(sigma=3., iters=10)
+def background_2D(image, sigma=3., iters=10, box_size=20, filter_size=10):
+    sigma_clip = SigmaClip(sigma=sigma, iters=iters)
     mask = (image == 0)
     bkg_estimator = MedianBackground()
-    bkg = Background2D(image,box_size=20, filter_size=10,
+    bkg = Background2D(image,box_size=box_size, filter_size=filter_size,
                    sigma_clip=sigma_clip, bkg_estimator=bkg_estimator,
                    mask=mask, edge_method=u'pad')
     print('Background Median: ' + str(bkg.background_median))
