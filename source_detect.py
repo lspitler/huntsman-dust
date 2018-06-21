a45e20b24d0d071d47111cb1bb20c01d9bf15ecd
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-21 20:21:00 +0530

    Drastic modifications! The code essentially functions the same as before, but now calls various functions instead of running a much longer cumbersome code

--- source_detect.py
+++ source_detect.py
@@ -1,74 +1,35 @@
 """Detecting and masking sources"""
 
 import os
-import glob
-
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-from astropy.io import fits
-from astropy.wcs import WCS
-from astropy.convolution import Gaussian2DKernel
-from astropy.stats import gaussian_fwhm_to_sigma
-from astropy.visualization import SqrtStretch
-from astropy.visualization.mpl_normalize import ImageNormalize
-from astropy.stats import SigmaClip
+from im_path import im_path
+from image_load import image_load
+from background_2D import background_2D
+from find_objects import find_objects
 
-from photutils import detect_threshold
-from photutils import detect_sources
-from photutils import Background2D, MedianBackground
-from photutils import deblend_sources
+# Finds fits file, reads it to import image data, header, WCS
+source_dir = '/Users/amanchokshi/Desktop/Huntsman/huntsman_dust'
+os.chdir(source_dir)
+image_path = im_path('/Users/amanchokshi/Desktop/Huntsman/Data')
+image, header, wcs = image_load(image_path)
 
-
-# Finds FITs files and created image_path
-cwd = os.getcwd()
-data_dir = '/Users/amanchokshi/Desktop/Huntsman/Data'
-os.chdir(data_dir)
-for file in glob.glob("*.fits"):
-    print(file)
-os.chdir(cwd)
-image_path = os.path.join(data_dir, file)
-
-# Read in FITs file
-hdulist = fits.open(image_path)
-
-# Extract image and header
-image = hdulist[0].data
-header = hdulist[0].header
-
-# Selects sub image
+# Selects sub region of image 
 image = image[1600:2300, 2000:3000].astype(float)
 
-# reads header and creates World Coordinate System (WCS) object
-w = WCS(header)
-
 # 2D background estimation
-sigma_clip = SigmaClip(sigma=3., iters=10)
-mask = (image == 0)
-bkg_estimator = MedianBackground()
-bkg = Background2D(image, (20, 20), filter_size=(10, 10),
-                   sigma_clip=sigma_clip, bkg_estimator=bkg_estimator,
-                   mask=mask, edge_method=u'pad')
-print(bkg.background_median)
-print(bkg.background_rms_median)
-plt.imshow(bkg.background, origin='lower', cmap='Greys')
-bkg.plot_meshes(outlines=True, color='#1f77b4')
-bkgrms = bkg.background_rms
-
+bkg, bkgrms = background_2D(image, sigma=3., iters=10, box_size=20, 
+                            filter_size=5)
 # detects a threshold of 3 sigma above background
-# threshold = detect_threshold(image, snr=3.)
 threshold = bkg.background + (3.0 * bkgrms)
 
-sigma = 2.0 * gaussian_fwhm_to_sigma    # FWHM = 2.
-kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
-kernel.normalize()
-segm = detect_sources(image, threshold, npixels=6, filter_kernel=kernel)
+# finds objects using image segmentation
+segm = find_objects(image, threshold, FWHM=2.0)
 
 
-norm = ImageNormalize(stretch=SqrtStretch())
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 7))
 ax1.imshow(image, origin='lower', cmap='Greys_r', norm=LogNorm())
 ax2.imshow(segm, origin='lower', cmap=segm.cmap(random_state=12345))
-
 
 plt.show()
