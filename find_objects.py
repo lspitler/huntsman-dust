e6be88b76123ec21506bf0b4a42ee286fafc93b6
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-21 20:18:10 +0530

    Function to find objects, using the umage segmentation technique from photutils

---
+++ find_objects.py
@@ -0,0 +1,12 @@
+"""Find objects using image segmentation"""
+
+from astropy.stats import gaussian_fwhm_to_sigma
+from astropy.convolution import Gaussian2DKernel
+from photutils import detect_sources
+
+def find_objects(image,threshold, FWHM=2.0):
+    sigma = FWHM * gaussian_fwhm_to_sigma
+    kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
+    kernel.normalize()
+    segm = detect_sources(image, threshold, npixels=6, filter_kernel=kernel)
+    return segm
