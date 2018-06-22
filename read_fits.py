a9a5bf030ec063b2a3a21bce6f578655f7747a53
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-23 00:17:08 +0530

    implemented plt_fits function. also added a updated WCS to account for smaller sub image

--- read_fits.py
+++ read_fits.py
@@ -4,29 +4,17 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
+
 from im_path import im_path
 from image_load import image_load
-
+from plt_fits import plt_fits
 
 source_dir = '/Users/amanchokshi/Desktop/Huntsman/huntsman_dust'
 os.chdir(source_dir)
-image_path = im_path('/Users/amanchokshi/Desktop/Huntsman/Data')
+image_path, file = im_path('/Users/amanchokshi/Desktop/Huntsman/Data')
 image, header, wcs = image_load(image_path)
 
+image = image[1600:2300, 2000:3000]
+wcs = wcs[1600:2300, 2000:3000]
 
-# Figure 1 - Linear
-fig_1 = plt.figure(1)
-fig_1.add_subplot(111, projection=wcs)
-plt.imshow(image, origin='lower', cmap='gray')
-plt.xlabel('RA')
-plt.ylabel('Dec')
-plt.colorbar()
-
-# Figure 2 - Logarithmic
-fig_2 = plt.figure(2)
-fig_2.add_subplot(111, projection=wcs)
-plt.imshow(image, origin='lower', cmap='gray', norm=LogNorm())
-plt.xlabel('RA')
-plt.ylabel('Dec')
-plt.colorbar()
-plt.show()
+plt_fits(image, wcs, title="Sample Title", cmap='Greys_r', norm=LogNorm())
