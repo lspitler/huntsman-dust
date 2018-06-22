b8b0e033f1e6fd6acefe400edd8ef5fd8a1711a4
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-22 08:19:28 +0530

    Added npix as an argument to the function. This is the minimum number of connected pixels for a group of pixels to be considered a source

--- find_objects.py
+++ find_objects.py
@@ -4,9 +4,10 @@ from astropy.stats import gaussian_fwhm_to_sigma
 from astropy.convolution import Gaussian2DKernel
 from photutils import detect_sources
 
-def find_objects(image,threshold, FWHM=2.0):
+
+def find_objects(image, threshold, FWHM=2.0, npixels=6):
     sigma = FWHM * gaussian_fwhm_to_sigma
     kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
     kernel.normalize()
-    segm = detect_sources(image, threshold, npixels=6, filter_kernel=kernel)
+    segm = detect_sources(image, threshold, npixels=npixels, filter_kernel=kernel)
     return segm
