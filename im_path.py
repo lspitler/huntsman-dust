48d41709784a6ad9954bf0ec4093f17e451fee13
Aman Chokshi <amanchokshi.iisc@gmail.com>
2018-06-23 00:13:55 +0530

    Modified to return file name as string stored in variable = file

--- im_path.py
+++ im_path.py
@@ -3,11 +3,12 @@
 import os
 import glob
 
+
 def im_path(data_dir):
     cwd = os.getcwd()
     os.chdir(data_dir)
     for file in glob.glob("*.fits"):
         print('File name: ' + file)
     os.chdir(cwd)
-    image_path = os.path.join(data_dir, file)        
-    return image_path
+    image_path = os.path.join(data_dir, file)
+    return image_path, file
